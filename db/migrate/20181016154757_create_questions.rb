d96d3efe2be6edc5c0a3bd8a3eeb7caaa9dd2a47
Matheus Perrut <matheus.perrut2302@gmail.com>
2018-10-17 07:17:25 -0300

    fk problem

--- db/migrate/20181016154757_create_questions.rb
+++ db/migrate/20181016154757_create_questions.rb
@@ -5,8 +5,6 @@ class CreateQuestions < ActiveRecord::Migration[5.2]
       t.string :explanation
       t.integer :quantity_answers
       t.references :quiz, foreign_key: true
-      t.references :answer, foreign_key: true
-      
 
       t.timestamps
     end
